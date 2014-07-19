c4932dd58c7e8ca5e56dcd263c979bd3be4b11b0
Johannes Lund <anviking@me.com>
2014-07-19 04:34:45 +0200

    Calculate delta of NSIndexSet

--- Chromatism/Chromatism/NSIndexSet+Intersection.swift
+++ Chromatism/Chromatism/NSIndexSet+Intersection.swift
@@ -18,5 +18,49 @@ extension NSIndexSet {
             })
         return finalSet
     }
+}
+
+func NSIndexSetDelta(oldSet: NSIndexSet?, newSet: NSIndexSet?) -> (additions: NSMutableIndexSet, deletions: NSMutableIndexSet) {
+    var deletions: NSMutableIndexSet
+    var additions: NSMutableIndexSet
     
+    // Old: ABC
+    // ∆(-)  B
+    // ∆(+)    D
+    // New: A CD
+    
+    // deletions = old - new
+    // additions = new - old
+    
+    // Since new and old values are optional, there are four possible cases
+    if let oldSet = oldSet {
+        if let newSet = newSet {
+            additions = newSet - oldSet
+            deletions = oldSet - newSet
+        } else {
+            additions = NSMutableIndexSet()
+            deletions = oldSet.mutableCopy() as NSMutableIndexSet
+        }
+    } else {
+        if let newSet = newSet {
+            additions = newSet.mutableCopy() as NSMutableIndexSet
+            deletions = NSMutableIndexSet()
+        } else {
+            additions = NSMutableIndexSet()
+            deletions = NSMutableIndexSet()
+        }
+    }
+    return (additions, deletions)
+}
+
+@infix func -(left: NSIndexSet, right: NSIndexSet) -> NSMutableIndexSet {
+    let indexSet = left.mutableCopy() as NSMutableIndexSet
+    indexSet.removeIndexes(right)
+    return indexSet
+}
+
+@infix func +(left: NSIndexSet, right: NSIndexSet) -> NSMutableIndexSet {
+    let indexSet = left.mutableCopy() as NSMutableIndexSet
+    indexSet.addIndexes(right)
+    return indexSet
 }
